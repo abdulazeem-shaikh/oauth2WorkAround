45e96c2fa1274ca7934c925af0726241833398bb
Abdul Azeem <abdulazeem.au@gmail.com>
2022-01-29 15:20:22 +0530

    first initialize

--- src/main/java/com/cms/AuthServerConfig.java
+++ src/main/java/com/cms/AuthServerConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 
 @Configuration
+@EnableAuthorizationServer
 public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
 
     private final PasswordEncoder passwordEncoder;
--- src/main/java/com/cms/TestController.java
+++ src/main/java/com/cms/TestController.java
@@ -6,23 +6,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import java.security.Principal;
 
 
 @RestController
 public class TestController {
-    @GetMapping("/test")
-    public String get(){
-
-        return "Welcome Abdul Azeem";
-    }
 
 
-
-    @ResponseBody
-    @RequestMapping("/test2")
-    public String get1(){
-
-
-        return "Welcome Abdul Azeem";
+    @GetMapping("/user/me")
+    public Principal user(Principal principal) {
+        return principal;
     }
 }
